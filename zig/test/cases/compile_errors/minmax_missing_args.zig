d7ce0fb29f05be0bdf95f25776797118b2097b23
bangbangsheshotmedown <apocalypse.praise@gmail.com>
2024-11-13 12:19:06 +0000

    use zig master

--- zig/test/cases/compile_errors/minmax_missing_args.zig
+++
@@ -1,9 +0,0 @@
-comptime { _ = @min(1); }
-comptime { _ = @max(1); }
-
-// error
-// backend=stage2
-// target=native
-//
-// :1:16: error: expected at least 2 arguments, found 1
-// :2:16: error: expected at least 2 arguments, found 1
